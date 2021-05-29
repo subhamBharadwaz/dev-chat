fbe6fdf8b6937ffcd2b2a99172784b9790a40d10
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2021-05-29 21:29:54 +0530

    js to jsx

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "@material-ui/core";
+
+const Header = () => {
+  return (
+    <Container maxWidth="sm">
+      <h1>Hello</h1>
+    </Container>
+  );
+};
+
+export default Header;
